0a71221da54ce6b75c57e9d16008eed8273d76a8
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-25 12:08:23 +0900

    写真サイズの修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full  min-h-screen flex justify-center items-center">
-      <div className="max-w-7xl w-full">
         <Hero />
-      </div>
     </main>
   );
 }
