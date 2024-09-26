0bfeb432ed6bbbb45eb8a868a78d44d60302dcae
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-26 18:11:16 +0900

    contentsのimgの挿入

--- components/Contents.tsx
+++ components/Contents.tsx
@@ -14,9 +14,74 @@ const Contents = () => {
         <p className="text-muted-foreground">
           マルシェや仕事、イベントなど、幅広いシーンで老若男女問わず利用できる場です。
         </p>
-      </div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
-        <Image src="/work.png" alt="Work Icon" height={200} width={300} />
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 mx-auto">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/tvwork.png"
+              alt="Work Icon"
+              layout="responsive"
+              height={200}
+              width={300}
+              className="object-cover"
+            />
+            <p className="mt-2 text-sm text-gray-600">リモートワークなどでの利用</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/work.png"
+              alt="Work Icon"
+              layout="responsive"
+              height={200}
+              width={300}
+              className="object-cover"
+            />
+            <p className="mt-2 text-sm text-gray-600">イベントスペースとしての利用</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/convesation.png"
+              alt="Work Icon"
+              layout="responsive"
+              height={200}
+              width={300}
+              className="object-cover"
+            />
+            <p className="mt-2 text-sm text-gray-600">会議やミーティングの場</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/art.png"
+              alt="Work Icon"
+              layout="responsive"
+              height={200}
+              width={300}
+              className="object-cover"
+            />
+            <p className="mt-2 text-sm text-gray-600">クリエイティブな活動</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/farming.png"
+              alt="Work Icon"
+              layout="responsive"
+              height={170}
+              width={200}
+              className="object-cover"
+            />
+            <p className="mt-2 text-sm text-gray-600">マルシェでの利用</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/studying.png"
+              alt="Work Icon"
+              layout="responsive"
+              height={200}
+              width={300}
+              className="object-cover"
+            />
+            <p className="mt-2 text-sm text-gray-600">学習や研究の場</p>
+          </div>
+        </div>
       </div>
     </section>
   );
