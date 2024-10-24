ab329ce72a0fce401fb3c49a04458c15d0b397fb
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-24 15:21:05 +0900

    form作成開始

--- app/recruit/page.tsx
+++ app/recruit/page.tsx
@@ -1,7 +1,19 @@
 export default function recruit() {
-    return (
-        <main>
-            <h2>recruit</h2>
-        </main>
-    )
+  return (
+    <main>
+      <div className=" max-w-[65%] mx-auto text-center">
+        <h1 className="font-extrabold text-xl md:text-4xl my-4">
+          お問い合わせ
+        </h1>
+        <p className="text-muted-foreground my-8">
+          弊社へのお問い合わせはこちらにお願いいたします。
+          <br />
+          3営業日以内にご返信させていただきます。
+        </p>
+        <div className="text-center mx-auto bg-blue-50">
+            <h1>aaaaa</h1>
+        </div>
+      </div>
+    </main>
+  );
 }
