cd9834d1074292347cf05c1713b878c37efa9d44
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-11-06 14:36:08 +0900

    googleマップの組み込み

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Fee from "@/components/Fee";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import { homeConfig } from "@/data/homeData";
-import Image from "next/image";
 
 export default function Home() {
   return (
