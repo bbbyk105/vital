e405f8032f164863575c67308f8a1b710acf2d37
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-12-13 15:00:07 +0900

    componentの振り分け

--- data/homeData.ts
+++ data/homeData.ts
@@ -1,38 +1,35 @@
 import { HomeConfig } from "@/types";
 
 export const homeConfig: HomeConfig = {
-    mainNav: [
-        
-        {
-            title: "ホーム",
-            href: "/"
-        },
+  mainNav: [
+    {
+      title: "ホーム",
+      href: "/",
+    },
 
-        {
-            title: "コンテンツ",
-            href: "#contents",
-        },
-        {
-            title: "施設紹介",
-            href: "#facility"
-        },
-        {
-            title: "料金",
-            href: "#fee"
-        },
-        {
-            title: "会社概要",
-            href: "#company-section"
-        },
-        {
-            title: "予約状況",
-            href: "/calendar"
-        },
-        {
-            title: "お問い合わせ",
-            href: "/contact"
-        }
-
-    ]    
-
-}
+    {
+      title: "コンテンツ",
+      href: "#contents",
+    },
+    {
+      title: "施設紹介",
+      href: "#facility",
+    },
+    {
+      title: "料金",
+      href: "/fee",
+    },
+    {
+      title: "会社概要",
+      href: "#company-section",
+    },
+    {
+      title: "予約状況",
+      href: "/calendar",
+    },
+    {
+      title: "お問い合わせ",
+      href: "/contact",
+    },
+  ],
+};
